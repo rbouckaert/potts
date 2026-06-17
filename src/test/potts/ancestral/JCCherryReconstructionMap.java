49091baa67601f062f3684bd706677d878af0810
Remco Bouckaert <higgs.ml@gmail.com>
2026-06-18 09:19:17 +1200

    JCCherryReconstructionMap polishing

--- src/test/potts/ancestral/JCCherryReconstructionMap.java
+++ src/test/potts/ancestral/JCCherryReconstructionMap.java
@@ -12,6 +12,7 @@ import beast.base.evolution.alignment.Sequence;
 import beast.base.evolution.likelihood.TreeLikelihood;
 import beast.base.evolution.sitemodel.SiteModel;
 import beast.base.evolution.substitutionmodel.JukesCantor;
+import beast.base.evolution.substitutionmodel.WAG;
 import beast.base.evolution.tree.Tree;
 import beast.base.inference.Runnable;
 import beastfx.app.tools.Application;
@@ -21,6 +22,10 @@ import test.beast.BEASTTestCase;
 @Description("Create heatmap from reconstruction at the root of a cherry tree with the same tip states")
 public class JCCherryReconstructionMap extends Runnable {
 	final public Input<OutFile> svgInput = new Input<>("svg", "svg file name to print heatmap to, ignored if not specified", new OutFile("[[none]]"));
+	final public Input<Double> detlaHInput = new Input<>("detlaH", "change in tree height at each step", 0.02);
+	final public Input<Integer> nrOfPStepsInput = new Input<>("nrOfPSteps", "number of steps in increasing P (deltap = 1/nrOfPSteps)", 100);
+	final public Input<Integer> nrOfHStepsInput = new Input<>("nrOfHSteps", "number of steps in increasing tree height (tree range = [0,..,nrOfHSteps*detlaH])", 100);
+	final public Input<Boolean> useAminoAcidInput = new Input<>("useAminoAcid", "flag to use Amino Acids with WAG instead of Nucleotides with Jukes Cantor", true);
 
 	@Override
 	public void initAndValidate() {
@@ -71,7 +76,7 @@ public class JCCherryReconstructionMap extends Runnable {
         double h = 1e-10;
         for (int x = 0; x < N; x++) {
         	tree.getRoot().setHeight(h);
-        	tree.getRoot().getLeft().setHeight(h/2);
+        	//tree.getRoot().getLeft().setHeight(h/2);
         	double p = 0;
         	for (int y = 0; y < M; y++) {
         		
@@ -82,7 +87,7 @@ public class JCCherryReconstructionMap extends Runnable {
         		seq2.initByName("taxon","B","value",seqProbs,"uncertain",true);
                         
                 Alignment data = new Alignment();
-                data.initByName("sequence", seq1, "sequence", seqFixed, "dataType", "nucleotide");
+                data.initByName("sequence", seq1, "sequence", seq2, "dataType", "nucleotide");
                 TreeLikelihood likelihood = new TreeLikelihood();
                 likelihood.initByName("useTipLikelihoods", true, "data", data, "tree", tree, "siteModel", siteModel, "scaling", TreeLikelihood.Scaling.none);
 
@@ -118,9 +123,107 @@ public class JCCherryReconstructionMap extends Runnable {
         return scores;
     }
         
-	@Override
+
+    public double[][] calcRootStateProbsAA() throws Exception {
+        N = nrOfPStepsInput.get();
+        deltah = detlaHInput.get();
+        M = nrOfHStepsInput.get();
+        deltap = 1.0/(M-1);
+
+        String seq1Probs = new String("1.,0.0,0.0,0.0;");
+        String seq2Probs = new String("1.,0.0,0.0,0.0;");
+        Sequence seq1 = new Sequence();
+		seq1.initByName("taxon","A","value",seq1Probs,"uncertain",true);
+		Sequence seq2 = new Sequence();
+		seq2.initByName("taxon","B","value",seq2Probs,"uncertain",true);
+
+		
+//		Sequence seqFixed = new Sequence();
+//		seqFixed.initByName("taxon","B","value","1.,0.0,0.0,0.0;","uncertain",true);
+//		seqFixed.initByName("taxon","B","value","0.5,0.161616,0.161616,0.161616;","uncertain",true);
+//		seqFixed.initByName("taxon","B","value","0.25,0.25,0.25,0.25;","uncertain",true);
+//		seqFixed.initByName("taxon","B","value","0.0,0.333333,0.333333,0.333333;","uncertain",true);
+		
+        Alignment data0 = new Alignment();
+        data0.initByName("sequence", seq1, "sequence", seq2, "dataType", "aminoacid");
+        
+        Tree tree = BEASTTestCase.getTree(data0, "(A:1.0,B:1.0)");
+
+        WAG WAG = new WAG();
+        WAG.initAndValidate();
+
+        SiteModel siteModel = new SiteModel();
+        siteModel.initByName("mutationRate", "1.0", "gammaCategoryCount", 1, "substModel", WAG);
+        
+        
+        double [][] scores = new double[M][N];
+        
+        double h = 1e-10;
+        for (int x = 0; x < N; x++) {
+        	tree.getRoot().setHeight(h);
+        	tree.getRoot().getLeft().setHeight(h/2);
+        	double p = 0;
+        	for (int y = 0; y < M; y++) {
+        		
+                String seqProbs = p+"";
+                String q = "," + (1-p)/19.0;
+                for (int a = 0; a < 19; a++) {
+                	seqProbs += q;
+                }
+                seqProbs += ";";
+
+//                String seqProbs = p*2/3+","+p/3;
+//                String q = "," + (1-p)/18.0;
+//                for (int a = 0; a < 18; a++) {
+//                	seqProbs += q;
+//                }
+//                seqProbs += ";";
+                
+                seq1 = new Sequence();
+        		seq1.initByName("taxon","A","value",seqProbs,"uncertain",true);
+        		seq2 = new Sequence();
+        		seq2.initByName("taxon","B","value",seqProbs,"uncertain",true);
+                        
+                Alignment data = new Alignment();
+                data.initByName("sequence", seq1, "sequence", seq2, "dataType", "aminoacid");
+                TreeLikelihood likelihood = new TreeLikelihood();
+                likelihood.initByName("useTipLikelihoods", true, "data", data, "tree", tree, "siteModel", siteModel, "scaling", TreeLikelihood.Scaling.none);
+
+                double logP = likelihood.calculateLogP();
+                double [] partials = likelihood.getRootPartials();
+                
+                double sum = 0;
+                for (double d : partials) {
+                	sum += d;
+                }
+                
+                //System.err.println(p + ":" + partials[0]/sum + ":" + Arrays.toString(partials) + " " + sum);
+                System.err.println(tree.getRoot().toNewick());
+            	scores[y][x] = partials[0]/sum;
+            	
+            	p += deltap;
+        	}
+        	h += deltah;
+        }
+        
+        for (int x = 0; x < N; x++) {
+        	double p = 0;
+        	for (int y = 0; y < M; y++) {
+        		//if (Math.abs(scores[y][x]-p) < deltap/2) {
+            	if (scores[y][x]>p) {
+        			scores[y][x] = - scores[y][x];
+        		}
+            	p += deltap;
+        	}
+        }
+        
+        
+        return scores;
+    }
+        
+    @Override
 	public void run() throws Exception {
-        double [][] scores = calcRootStateProbs();
+        double [][] scores = useAminoAcidInput.get() ? calcRootStateProbsAA() : calcRootStateProbs();
                 
         if (!svgInput.get().getName().equals("[[none]]")) {
         	Log.info("Writing to file " + svgInput.get().getPath());
